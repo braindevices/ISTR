520b0d410ba8be5dbf53971d962b0bfcf072a7c0
Ling Wang@g731gw <LingWangNeuralEng@gmail.com>
2021-12-17 14:37:06 +0100

    fix the components path

--- projects/ISTR/istr/config.py
+++ projects/ISTR/istr/config.py
@@ -1,6 +1,8 @@
+import os
 from detectron2.config import CfgNode as CN
 
-
+_src_dir = os.path.dirname(__file__)
+_ISTR_dir = os.path.realpath(os.path.dirname(_src_dir))
 def add_ISTR_config(cfg):
     """
     Add config for ISTR.
@@ -49,4 +51,4 @@ def add_ISTR_config(cfg):
     cfg.MODEL.ISTR.IOU_LABELS = [0, 1]
 
     # Encoder
-    cfg.MODEL.ISTR.PATH_COMPONENTS = "./projects/ISTR/LME/coco_2017_train_class_agnosticTrue_whitenTrue_sigmoidTrue_60_siz28.npz"
+    cfg.MODEL.ISTR.PATH_COMPONENTS = f"{_ISTR_dir}/LME/coco_2017_train_class_agnosticTrue_whitenTrue_sigmoidTrue_60_siz28.npz"
